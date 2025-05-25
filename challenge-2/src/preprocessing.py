079d29a5bb31a83961dd1391209c85bca4290a3c
Antik Sen <antik.sen.kgp@gmail.com>
2025-05-25 19:52:07 +0530

    Update preprocessing.py

--- challenge-2/src/preprocessing.py
+++ challenge-2/src/preprocessing.py
@@ -3,6 +3,6 @@
 Author: Annam.ai IIT Ropar
 Team Name: ARiES
 Team Members: Aditya Pratap Singh Takuli, Piyush Gupta, Lay Gupta, Antik Sen, Satarupa Mishra
-Leaderboard Rank: <18>
+Leaderboard Rank: 18
 
 """
