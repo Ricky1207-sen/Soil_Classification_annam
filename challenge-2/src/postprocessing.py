1e018f5d9329b7b0375bfa24c93d0848ee7eb769
Antik Sen <antik.sen.kgp@gmail.com>
2025-05-25 19:51:38 +0530

    Update postprocessing.py

--- challenge-2/src/postprocessing.py
+++ challenge-2/src/postprocessing.py
@@ -1,8 +1,8 @@
 """
 
 Author: Annam.ai IIT Ropar
-Team Name: SoilClassifiers
-Team Members: 
+Team Name: ARiES
+Team Members: Aditya Pratap Singh Takuli, Piyush Gupta, Lay Gupta, Antik Sen, Satarupa Mishra
 Leaderboard Rank: 18
 
 """
