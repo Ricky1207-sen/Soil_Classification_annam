ddfbeef2bb76b8b57b7372fd5b9c8812a46bfa83
Satarupa Mishra <mishrasatarupa360@gmail.com>
2025-05-25 23:43:06 +0530

    Update postprocessing.py

--- challenge-1/src/postprocessing.py
+++ challenge-1/src/postprocessing.py
@@ -1,3 +1,11 @@
+"""
+
+Author: Annam.ai IIT Ropar
+Team Name: ARiES
+Team Members: Aditya Pratap Singh Takuli, Piyush Gupta, Lay Gupta, Antik Sen, Satarupa Mishra
+Leaderboard Rank: 29
+
+"""
 import pandas as pd
 
 # Mapping from numeric IDs back to soil type labels
